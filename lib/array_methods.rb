6113932c431c81682178e456ec74350de33fd19e
Adrian Samaniego <llathander@gmail.com>
2020-01-22 19:40:47 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,9 @@
 def find_element_index(array, value_to_find)
   array.length.times do |x|
     if array|x| == value_to_find
-  return x
+  return find_max_value
+  else return NIL
+end
 end
 
 def find_max_value(array)
